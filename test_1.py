bb82e24a386533ae4519bdace6a878085cad9440
billioniareman <93970384+billioniareman@users.noreply.github.com>
2022-03-02 17:19:59 +0530

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -1,19 +1,17 @@
-# this is an automation test on the amazon 
-# we are going to explore the selenium framework with python 
+#this is the automation code to get earbuds details 
+
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
-product_name=input("enter the product name ")
-brand_name=input("enter the name of brand ")
-min,max=input("enter range of price(in dollars)").split('-')
-url="https://www.amazon.com"
 driver=webdriver.Chrome("C:\webdriver\chromedriver.exe")
-driver.get(url)
-driver.find_element_by_xpath('//*[@id="twotabsearchtextbox"]').send_keys(product_name," ",brand_name)
-driver.find_element_by_xpath('//*[@id="nav-search-submit-button"]').click()
-driver.find_element_by_xpath('//*[@id="low-price"]').send_keys(min)
-driver.find_element_by_xpath('//*[@id="high-price"]').send_keys(max)
-driver.find_element_by_xpath('//*[@id="a-autoid-1"]/span/input').click()
+driver.get("https://www.amazon.com")
+class get_product():
+    def product(self):
 
-lst=driver.find_elements_by_xpath('//*[@id="brandsRefinements"]/ul')
-print(lst)
+        value=driver.find_element_by_xpath("//input[@id='twotabsearchtextbox']").send_keys("earbuds")
+        driver.find_element_by_xpath("//input[@id='nav-search-submit-button']").click()
+
+obj1=get_product()
+obj1.product()
